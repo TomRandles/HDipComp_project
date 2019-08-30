76d475a8ecc8385d4bd2a476c3061544d00e2a8c
TomRandles <46902723+TomRandles@users.noreply.github.com>
2019-08-30 14:32:53 +0100

    Delete SQLQuery4b.sql

--- SQLQuery4b.sql
+++
@@ -1,9 +0,0 @@
-﻿INSERT INTO PROGRAMMES (ProgrammeID, ProgrammeName, ProgrammeDescription,  ProgrammeCredits, ProgrammeCost, ProgrammeQQILevel, LastUpdated)
-        VALUES ('BS0501', 'Business Studies', 'Business Studies', 45, 450.00, 5, '03/07/2019'),
-               ('CP0502', 'Computer Programming','Computer Programming', 90, 450.00, 5, '03/07/2019'),
-	           ('BM0601', 'Business Management', 'Business Management', 45, 450.00, 6, '03/07/2019'),
-	           ('OA0502', 'Office Admin','Office Admin', 45, 450.00, 5, '03/07/2019'),
-	           ('SS0501', 'Sports Science','Sports Science', 90, 400.00, 5, '03/07/2019');
-
-select * from students
-update Students set County = 'Antrim' Where StudentID='S490185';
