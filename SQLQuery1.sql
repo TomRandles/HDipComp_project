0c25c10d6633033efa7b8771ebe19668661d39c2
TomRandles <46902723+TomRandles@users.noreply.github.com>
2019-08-30 14:28:00 +0100

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,4 +0,0 @@
-﻿select * from Students where ProgrammeID='SS0501';
-update Students SET MobilePhoneNumber = '0860481969' where StudentID='G539378'
-
-update Students SET EmailAddress = 'Tom.Randles@mail.ITSligo.ie' where StudentID='G558980'
