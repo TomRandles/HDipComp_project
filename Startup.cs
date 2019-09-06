090c0029354685c43a93fb27552e296b0d96fdcb
Tom Randles <randles.tom@gmail.com>
2019-09-06 18:22:20 +0100

    Misc updates

--- Startup.cs
+++ Startup.cs
@@ -66,6 +66,10 @@ namespace TRHDipComp_Project
                 app.UseHsts();
             }
 
+            app.UseExceptionHandler("/MyErrorPage");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
